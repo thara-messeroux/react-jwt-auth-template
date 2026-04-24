b4f9adeda6c827856564fd0d407085775a708e3d
thara-messeroux <tharamesseroux@gmail.com>
2026-04-23 21:26:25 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>Hello, friend!</h1>
+    </div>
+  );
+};
+
+export default App;
